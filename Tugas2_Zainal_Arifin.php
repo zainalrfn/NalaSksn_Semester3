4ff5ecff3672d2756af656f2576896718626c3d2
Nalasksn <za036335@gmail.com>
2023-10-19 13:30:56 +0700

    Mengerjakan Modul

---
+++ Tugas2_Zainal_Arifin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Tugas2</title>
+</head>
+<body>
+<h3>SELAMAT DATANG DI WEBSITE RPL POLIBANG JEPARA</h3>
+<p>Ayo kita belajar Web Developer bersama</p>
+<p>Zainal Arifin</p>
+<p>Sukosono Kedung</p> 
+<a href=https://www.polibang.ac.id>Link ke Polibang</a><br>
+<a href=https://qwords.com/blog/pemrograman-web>Link ke Belajar pemrograman</a>
+</body>
+</html>
